fea28d4e785924670980ff6cb1280f0ef45dee7a
Jonna Gustafsson <jonnaagustafssoon@hotmail.com>
2024-03-18 14:51:22 +0100

    Navbar
    Grunden till navigeringsmenyn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/login.js'; // Importera inloggningssidan
-import OverviewTable from './pages/overview'; // Importera översiktstabellen
+import Login from './pages/login.js'; 
+import OverviewTable from './pages/overview'; 
 import Timereport from './pages/Timereport.js';
+import NavigationMenu from './components/Navbar.js';
 import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <NavigationMenu />
        <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/overview" element={<OverviewTable/>} />
