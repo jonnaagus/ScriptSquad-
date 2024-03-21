d4dd56cebbf9f905c9b5c7df98d2d2b982ce77b6
Saibot155 <tobias.soderqvist@hotmail.com>
2024-03-21 10:37:24 +0100

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,9 @@
 import lockImage from "../images/lock.png"
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 import "../styles/login.css"; // Importera inloggningslayout
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
-import { Link, useNavigate, useLocation, isRouteErrorResponse } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 function SaveUser(user) {
@@ -12,19 +11,12 @@ function SaveUser(user) {
     window.localStorage.setItem("user", user);
 }
 
-
 function loadUser() {
     var user = window.localStorage.getItem("user");
     return JSON.parse(user);
 }
 
-function loadPeople() {
-    var user = window.localStorage.getItem("people");
-    return user;
-
-}
 export default function Login() {
-
 
     const { setAuth } = useAuth();
     const navigate = useNavigate();
@@ -33,45 +25,53 @@ export default function Login() {
     //set location user tried to login from (broken: looses location when returning from notions login page)
     const from = location.state?.from?.pathname || "/";
 
-    console.log(from)
-
     //bool to stop fetch from running twice at the same time
     var isFetching;
 
+    //used to display loading if testing localstorage user
+    const [validating, setValidating] = useState(false);
+    
     // The OAuth client ID from the integration page!
     const oauth_client_id = "d0b58e75-fbe9-4e66-9e2b-c613671e6a6e";
 
     async function GetUser() {
 
         //if user is set in local storage login and send to page user tried to access
-        if(loadUser() != null){
+        if (loadUser() != null) {
             const temp = loadUser()
             const accessToken = temp.accessToken
             const userName = temp.userName
-            const  email = temp.email
+            const email = temp.email
 
-            const response = await axios.get(`http://localhost:3002/validate/${accessToken}`)
+            try {
+                setValidating(true);
+                //send access token in localstorage to see if user is in workspace
+                const response = await axios.get(`http://localhost:3002/validate/${accessToken}`)
 
-            console.log(response.data);
-
-            if(response.data.bot.owner.user.person.email = email){
-                console.log("MATCH")
+                //if user is in workspace compare email and send to desired page
+                if (response.data.bot.owner.user.person.email === email) {
+                    await setAuth({ accessToken, userName, email })
+                    navigate(from, { replace: true });
+                    setValidating(false);
+                    console.log(false)
+                }
+                //if email dont match user in workspace clear user from localstorage and send to login page
+                else {
+                    localStorage.clear();
+                    navigate("/");
+                    setValidating(false);
+                    console.log(false)
+                }
+                //if no respone from server clear and send to login page
+            } catch (error) {
+                console.log("clear")
+                localStorage.clear();
+                navigate("/");
+                setValidating(false);
             }
-            else{
-                console.log("no match")
-            }
 
-           await setAuth({accessToken, userName, email})
-            navigate(from, { replace: true });
+
         }
-
-
-
-
-
-
-
-
 
         //get code from url when returning from notion login page
         const params = new URL(window.document.location).searchParams;
@@ -107,8 +107,6 @@ export default function Login() {
                 console.log("USer not auth")
             }
         }
-
-        // console.log(JSON.stringify(response?.data));
     }
 
 
@@ -117,7 +115,7 @@ export default function Login() {
     // When you open the app, this doesn't do anything, but after you sign into Notion, you'll be redirected back with a code at which point we call our backend.
     useEffect(() => {
         GetUser();
-    }, []);
+    });
 
 
     return (
@@ -127,32 +125,38 @@ export default function Login() {
                 <h1 className="outfit-font">Projekt.se</h1>
             </header>
 
-            <div className="image-container">
-                <img src={lockImage} alt="lock" style={{ width: '100px', height: 'auto' }} />
-            </div>
-            {/* <!--- Bestämmer plats på rutan--> */}
-            <div className="container">
-                {/* <!--- Forma rutan--> */}
-                <div className="login-box">
-                    <h2>Logga in med <span className="notion-logo">Notion</span></h2>
+            {validating && <p>LADDAR....</p>}
 
-                    {loadUser() != null ?
-                        <div>
-                            <div>
-                                <p> Du är inloggad som {loadUser().userName}</p>
-                                <a onClick={() => localStorage.clear()} href="http://localhost:3000/">Logga ut</a>
-                            </div>
-                        </div>
-                        :
-                        <a
-                            id="notion-login-button"
-                            href={`https://api.notion.com/v1/oauth/authorize?client_id=${oauth_client_id}&response_type=code&owner=user`}
-                        >
-                            Ta mig till Notion
-                        </a>
-                    }
+            {!validating && 
+            <div>
+                <div className="image-container">
+                    <img src={lockImage} alt="lock" style={{ width: '100px', height: 'auto' }} />
                 </div>
-            </div>
+                    {/* <!--- Bestämmer plats på rutan--> */}
+                    <div className="container">
+                        {/* <!--- Forma rutan--> */}
+                        <div className="login-box">
+                            <h2>Logga in med <span className="notion-logo">Notion</span></h2>
+
+                            {loadUser() != null ?
+                                <div>
+                                    <div>
+                                        <p> Du är inloggad som {loadUser().userName}</p>
+                                        <a onClick={() => localStorage.clear()} href="http://localhost:3000/">Logga ut</a>
+                                    </div>
+                                </div>
+                                :
+                                <a
+                                    id="notion-login-button"
+                                    href={`https://api.notion.com/v1/oauth/authorize?client_id=${oauth_client_id}&response_type=code&owner=user`}
+                                >
+                                    Ta mig till Notion
+                                </a>
+                            }
+                        </div>
+                    </div>
+                </div>
+            }
 
             <footer>
                 {/* <!-- Footer --> */}
