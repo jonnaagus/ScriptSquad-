799718bf95dbb4d2ee3d47cd0dc688efe4cfefc8
Jonna Gustafsson <jonnaagustafssoon@hotmail.com>
2024-03-21 12:44:51 +0100

    Layout
    Layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.js';
 import Login from './pages/login.js'; 
 import OverviewTable from './pages/overview'; 
 import Timereport from './pages/Timereport.js';
-import NavigationMenu from './components/Navbar.js';
-import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavigationMenu />
-       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/overview" element={<OverviewTable/>} />
-        <Route path="/timereport" element={<Timereport/>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Login />} />
+        <Route path="/overview" element={<OverviewTable />} />
+        <Route path="/timereport" element={<Timereport />} />
+      </Route>
+    </Routes>
   );
 }
 
